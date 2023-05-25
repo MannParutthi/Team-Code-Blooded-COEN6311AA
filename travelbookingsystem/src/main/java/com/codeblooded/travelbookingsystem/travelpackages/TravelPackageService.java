091aa4dac97cd9140f995907664614bd5a5ddc74
MannParutthi <manan.paruthi@gmail.com>
2023-05-25 10:58:05 -0400

    Added Activities backend code in Create Travel Packages Section.

--- travelbookingsystem/src/main/java/com/codeblooded/travelbookingsystem/travelpackages/TravelPackageService.java
+++ travelbookingsystem/src/main/java/com/codeblooded/travelbookingsystem/travelpackages/TravelPackageService.java
@@ -8,6 +8,36 @@ import java.util.List;
 
 @Service
 public class TravelPackageService {
+    private List<TravelPackage> packages = new ArrayList<>(Arrays.asList(
+        new TravelPackage(
+                "Toronto",
+                "Canada",
+                5,
+                1050,
+                TravelPackage.PackageType.PREMADE,
+                50005,
+                List.of(60001, 60002, 60003),
+                List.of(new HotelDaysWithId(40001, 2), new HotelDaysWithId(40004, 3))),
+        new TravelPackage(
+                "Vancouver",
+                "Canada",
+                5,
+                2060,
+                TravelPackage.PackageType.PREMADE,
+                50001,
+                List.of(60004, 60005, 60006),
+                List.of(new HotelDaysWithId(40009, 2), new HotelDaysWithId(40010, 3))),
+        new TravelPackage(
+                "Montreal",
+                "Canada",
+                5,
+                2550,
+                TravelPackage.PackageType.PREMADE,
+                50003,
+                List.of(60007, 60008, 60009),
+                List.of(new HotelDaysWithId(40002, 2), new HotelDaysWithId(40006, 3)))
+));
+
     public static final String PKG_ALREADY_EXISTS = "Package Already Exists";
     public static final String PKG_CREATED_SUCCESSFULLY = "Package Created Successfully !";
     public static final String TRAVEL_PACKAGE_NOT_FOUND = "Travel Package Not Found !";
