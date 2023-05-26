bb247c7d4e854aaff78bfee73dbadc9dc1bc3036
Anant Shukla <accounts@anantshukla.com>
2023-05-26 17:18:51 -0400

    Added API Management in UI along with logo on header

--- travel-booking-system-client/src/app/create-travel-package/create-travel-package.service.ts
+++ travel-booking-system-client/src/app/create-travel-package/create-travel-package.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CreateTravelPackageService {
 
-  private baseUrl = "http://localhost:8081/api/v1";
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
