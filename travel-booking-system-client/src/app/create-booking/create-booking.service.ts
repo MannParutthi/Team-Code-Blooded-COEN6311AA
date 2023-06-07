6daa3829317c0b816c514edb56e0a9539b5336e6
Anant Shukla <accounts@anantshukla.com>
2023-06-07 13:20:46 -0400

    Updated the url for API server for new components

--- travel-booking-system-client/src/app/create-booking/create-booking.service.ts
+++ travel-booking-system-client/src/app/create-booking/create-booking.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateBookingService {
-  private baseUrl = "http://localhost:8081/api/v1";
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
